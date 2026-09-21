fc9f7188a1c4bad275f8094209c91f8888bc6999
Amit Patil <amitpatil49492@gmail.com>
2026-09-21 15:24:59 +0530

    Changes Added

---
+++ Type_Conversion/Assignments/Assignment_1/Tester.java
@@ -0,0 +1,12 @@
+package Type_Conversion.Assignments.Assignment_1;
+
+public class Tester {
+    public static void main(String[]args){
+        float pi=3.14f;
+        int radius=4;
+        float area=radius*radius*pi;
+
+        System.out.println("Area of circle: "+area);
+    }
+    
+}
