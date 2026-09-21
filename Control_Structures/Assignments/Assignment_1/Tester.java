781344827a189e08523258e86f5cbdec36fa3813
Amit Patil <amitpatil49492@gmail.com>
2026-09-21 16:04:22 +0530

    File Added

---
+++ Control_Structures/Assignments/Assignment_1/Tester.java
@@ -0,0 +1,16 @@
+package Control_Structures.Assignments.Assignment_1;
+
+public class Tester {
+    public static void main(String[]args){
+        int num_1=6;
+        int num_2=5;
+
+        if(num_1==num_2){
+            System.out.println("Numbers are same, addition of numbers is: "+(num_1+num_2));
+        }
+        else if(num_1!=num_2){
+            System.out.println("Numbers are not same, double of addition is: "+(num_1+num_2)*2);
+        }
+    }
+    
+}
